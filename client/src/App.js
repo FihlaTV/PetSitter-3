f5e5c866b40ccba90da6ef691ac2e1b6237c2317
CristianCustodio <cristian325@gmail.com>
2018-10-04 19:31:13 -0400

    Added Jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jumbotron from './components/Jumbotron';
 
 class App extends Component {
 
@@ -26,12 +27,19 @@ class App extends Component {
 
   render() {
     return (
+     
+
       <div className="App">
-        <header className="App-header">
+
+      <Jumbotron>
+        <h1>This is Nat's Jumbotron</h1>
+      </Jumbotron>
+      
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        <p className="App-intro">{this.state.response}</p> */}
       </div>
     );
   }
