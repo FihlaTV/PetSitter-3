da2fedffc5c6adbd75b05cf70014d731c9247975
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-10-05 23:42:55 -0400

    SignIn page working with components

--- client/src/pages/SignIn/SignIn.js
+++ client/src/pages/SignIn/SignIn.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "../../components/Button";
+import { Col, Row, Container } from "../../components/Grid"; 
 
-const SignIn = () => (
+
+class SignIn extends Component {
+
+    render() {
+        return (
+
+            <Container fluid>
+                <Row>
+                    <Col size="md-10">
+                        <h1>Sign-In Page</h1>
+                        <p>
+                            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
+                            ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
+                            quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+                            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
+                            diam, sit amet facilisis lectus blandit at.
+                        </p>
+                        <Button>
+                            Test Button
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+/* const SignIn = () => (
     <div>
         <h1>Sign In Page</h1>
         <p>
@@ -10,7 +39,8 @@ const SignIn = () => (
             imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
             diam, sit amet facilisis lectus blandit at.
         </p>
-    </div>   
-);
+        <Button>Testing</Button>
+    </div>
+); */
 
 export default SignIn;
