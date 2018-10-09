a4218377ad32da4244fa8ba3e944e04a3da12e0b
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-10-09 00:01:01 -0400

    added card to favorites page

--- client/src/pages/Favorites/Favorites.js
+++ client/src/pages/Favorites/Favorites.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { Col, Row } from "../../components/Grid";
 import { ProfilePhoto } from "../../components/Profile";
 import { Image, Button, Grid, Container, Icon, Rating } from "semantic-ui-react"
+import { Card, CardBody } from 'mdbreact';
 import headerimage from "../Search/headerimage.jpg";
 import "./Favorites.css"
 
@@ -32,30 +33,40 @@ class Favorites extends Component {
                     </Row>
                     <Container>
                         <Row>
-                            <Col size="sm-12 md-3">
-                                <ProfilePhoto />
+                            <Col size="sm-12 md-12">
+                                <Card className="w-100 z-depth-5" id="favoriteCard">
+                                    <CardBody>
+                                        <Row>
+                                            <Col size="sm-12 md-3">
+                                                <ProfilePhoto />
+                                            </Col>
+                                            <Col size="sm-12 md-3">
+                                                <h2>John Doe, 24</h2>
+                                                <br />
+                                                <Rating maxRating={5} defaultRating={3} icon='star' size='massive' />
+                                            </Col>
+                                            <Col size="sm-12 md-3" className="align-middle">
+                                                <h1>$29</h1><br/>
+                                                <h3>per night</h3>
+                                            </Col>
+                                            <Col size="sm-12 md-3">
+                                                <Button animated id="contactsitterButton">
+                                                    <Button.Content visible>Contact Pet Sitter</Button.Content>
+                                                    <Button.Content hidden>
+                                                        <Icon name='mail' />
+                                                    </Button.Content>
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </CardBody>
+                                </Card>
                             </Col>
-                            <Col size="sm-12 md-3">
-                                <h2>John Doe, 24</h2>
-                                <br />
-                                <Rating maxRating={5} defaultRating={3} icon='star' size='massive' />
-                            </Col>
-                            <Col size="sm-12 md-3">
-                                <h1>$29 per night</h1>
-                            </Col>
-                            <Col size="sm-12 md-3">
-                                <Button animated id="contactsitterButton">
-                                    <Button.Content visible>Contact Pet Sitter</Button.Content>
-                                    <Button.Content hidden>
-                                        <Icon name='mail' />
-                                    </Button.Content>
-                                </Button>
-                            </Col>
+                        </Row>
+                        <Row>
+                            <br /> <br /> <br /> <br />
                         </Row>
                     </Container>
                 </Grid>
-
-
             </div>
         )
     }
