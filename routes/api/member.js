98e3c7ed4059554e82d30fbcb99a04ef03a67d17
Cristian Custodio <cristian325@gmail.com>
2018-10-17 22:21:14 -0400

    Revert "Merge pull request #26 from cristian-custodio/joseph"
    This reverts commit e38acd09c484ea6e37c3f51265f822bc4931e978, reversing
    changes made to 13835bf7d94d8810885b6a8fc832173e7beea52f.

--- routes/api/member.js
+++ routes/api/member.js
@@ -13,116 +13,114 @@ const validateRegisterInput = require('../../helpers/validators/register');
 const validateLoginInput = require('../../helpers/validators/login');
 
 //Authentication Routes
-router.post('/register', (req, res) => {
+router.post('/register', (req, res) =>  {
     // Check validation on request body 
     const { errors, isValid } = validateRegisterInput(req.body);
     if (!isValid) {
         return res.status(400).json(errors);
     }
 
-    // deconstruct values from body 
-    const { email, name, password } = req.body
-    Member.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                errors.email = 'email already registered'
-                return res.status(409).json(errors);
-            } else {
-                const avatar = gravatar.url(email, {
-                    s: '200', //the size
-                    r: 'pg', //rating
-                    d: 'mm' //default
-                });
+     // deconstruct values from body 
+     const { email, name, password } = req.body
+     Member.findOne({email: email})
+     .then(user => {
+       if (user) {
+         errors.email = 'email already registered'
+         return res.status(409).json(errors);
+       } else {
+         const avatar = gravatar.url(email, {
+           s: '200', //the size
+           r: 'pg', //rating
+           d: 'mm' //default
+         });
 
-                //es6 short hand for name:name, email:email etc.
-                const newMember = new Member({
-                    name,
-                    email,
-                    avatar,
-                    password
-                });
-                newMember.save()
-                    .then(member => res.status(201).json(member))
-                    .catch(err => {
-                        console.log(err)
-                        erros.db = 'an error has occured in saving in the db'
-                        res.status('500').json(erros)
-                    });
-            }
-        });
+         //es6 short hand for name:name, email:email etc.
+         const newMember = new Member({
+           name,
+           email,
+           avatar,
+           password
+         });
+         newMember.save()
+         .then(member => res.status(201).json(member))
+         .catch(err => {
+           console.log(err)
+           erros.db = 'an error has occured in saving in the db'
+           res.status('500').json(erros)
+         });
+       }
+     });
 });
 
 router.post('/login', (req, res) => {
-    // Check validation on request body 
-    const { errors, isValid } = validateLoginInput(req.body);
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    const { email, password } = req.body
-    //short hand for email:email
-    Member.findOne({ email })
-        .then(user => {
-            //check for user 
-            if (!user) {
-                errors.user = 'User not found';
-                return res.status(404).json(errors);
-            }
-            user.comparePassword(password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        const payload = { id: user.id, name: user.name, avatar: user.avatar } // created JWT payload
-                        jwt.sign(
-                            payload,
-                            keys.secretOrPrivateKey,
-                            { expiresIn: 7200 },
-                            (err, token) => {
-                                res.json({
-                                    sucess: true,
-                                    token: 'Bearer ' + token
-                                });
-                            });
-                    } else {
-                        errors.password = 'Password incorrect';
-                        return res.status(400).json(errors)
-                    }
-                })
-                .catch(err => {
-                    console.log(err)
-                    res.status('500').json({ login: 'an error has occured in the login process' })
-                });
+  // Check validation on request body 
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) {
+      return res.status(400).json(errors);
+  }
+  const { email, password } = req.body
+  //short hand for email:email
+  Member.findOne({ email })
+      .then(user => {
+          //check for user 
+          if (!user) {
+              errors.user= 'User not found';
+              return res.status(404).json(errors);
+          }
+          user.comparePassword(password)
+              .then(isMatch => {
+                  if (isMatch) {
+                      const payload = { id: user.id, name: user.name, avatar: user.avatar } // created JWT payload
+                      jwt.sign(
+                          payload,
+                          keys.secretOrPrivateKey,
+                          { expiresIn: 7200 },
+                          (err, token) => {
+                              res.json({
+                                  sucess: true,
+                                  token: 'Bearer ' + token
+                              });
+                          });
+                  } else {
+                      errors.password = 'Password incorrect';
+                      return res.status(400).json(errors)
+                  }
+              })
+              .catch(err => {
+                  console.log(err)
+                  res.status('500').json({ login: 'an error has occured in the login process' })
+              });
 
-        });
+      });
 });
 
 //Information Routes
-router.get("/:id", (req, res) => {
-    db.Member
-        .find({ _id: req.params.id })
-        .then(member => res.status(200).json(member))
-        .catch(err => res.status(422).json(err))
-})
+router.route("/:id")
+  .get(function(req, res){
+      db.Member.find({_id:req.params.id}).then(function(member){   
+        res.json({msg: `${member.name}  logged in`});
+      })
+  })
 
-// Add member favorites
-router.route("/addFavorite/:memberId/:petSitterId")
-    .put(function (req, res) {
-        db.PetSitter.findOne({ _id: req.params.petSitterId }).then(function (sitterId) {
+  router.route("/addFavorite/:memberId/:petSitterId")
+    .put(function(req, res){
+        db.PetSitter.findOne({_id:req.params.petSitterId}).then(function(sitterId){
+            // var petSitter =  res.json();
             db.Member.updateOne(
-                { "_id": req.params.memberId },
+                { "_id": req.params.memberId},
                 { "$push": { "favorites": sitterId } },
                 function (err, raw) {
-if (err) return handleError(err);
-console.log('The raw response from Mongo was ', raw);
+                    if (err) return handleError(err);
+                    console.log('The raw response from Mongo was ', raw);
                 })
         })
     })
-
-//search members favorites    
-router.get("/memberFavorites/:id", (req, res) => {
-    db.Member
-        .findOne({ _id: req.params.id })
-        .then(memberFavorites => res.status(500).json(memberFavorites.favorites))
-        .catch(err => res.status(422).json(err))
-})
+    router.route("/memberFavorites/:id")
+        .get(function (req, res){
+            db.Member.findOne({_id:req.params.id}).then(function(memberFavorites){
+                res.json(memberFavorites.favorites)
+            })
+        })
 
 
-module.exports = router;
+  module.exports = router;
