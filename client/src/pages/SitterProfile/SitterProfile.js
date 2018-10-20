2b8c6e6611bb677c32bc2fa77684ee20482c8232
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-10-19 21:36:51 -0400

    profile page completed

--- client/src/pages/SitterProfile/SitterProfile.js
+++ client/src/pages/SitterProfile/SitterProfile.js
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import { Col, Row } from "../../components/Grid";
 import { Carousel, CarouselInner, CarouselItem, CarouselCaption, View, Mask } from 'mdbreact';
 import { Grid, Image, Rating, Icon, Button } from "semantic-ui-react";
-import { Card, CardBody, CardHeader } from 'mdbreact';
+import { Card, CardBody, CardHeader, Modal, ModalHeader, ModalBody, Fa, ModalFooter, Badge } from 'mdbreact';
 import carousel01 from "../../components/Header/carousel01.jpg";
 import carousel02 from "../../components/Header/carousel02.jpg";
 import carousel03 from "../../components/Header/carousel03.jpg";
 import "./SitterProfile.css";
 
 class SitterProfile extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            modal: false
+        };
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
 
     render() {
         console.log(this.props);
@@ -72,13 +85,13 @@ class SitterProfile extends Component {
 
                     <Row>
                         {/* Left Column */}
-                        <Col size="sm-12 md-5" style={{ textAlign: "center" }}>
+                        <Col size="sm-12 md-5" style={{ textAlign: "center" }} id="leftColumn">
 
                             <p><Image src={this.props.location.state.profilePhoto} alt="profile pic" size="medium" className="img-fluid hoverable mx-auto d-block" circular /></p><br />
                             <Rating maxRating={5} defaultRating={this.props.location.state.rating} icon='star' size='massive' disabled /><br />
                             <h2 id="pricetitle">Price: ${this.props.location.state.price}/night</h2>
                             <span id="AddtoFavorites"><Icon circular inverted name='heart' size="small" color='red' />Add to Favorites</span><br /><br /><br />
-                            <Button animated id="contactsitterButton" className="mx-auto d-block">
+                            <Button animated id="contactsitterButton" className="mx-auto d-block" style={{ marginBottom: "100px" }} onClick={this.toggle}>
                                 <Button.Content visible>Contact {this.props.location.state.name}</Button.Content>
                                 <Button.Content hidden>
                                     <Icon name='mail' />
@@ -89,7 +102,7 @@ class SitterProfile extends Component {
                         {/* Right Column */}
                         <Col size="sm-12 md-7" style={{ textAlign: "center" }}>
                             <Card className="z-depth-5" id="favoriteCard">
-                            <CardHeader style={{ background: "#0d2c38", color: "#ffffff"}}>Why You Should Hire Me</CardHeader>
+                                <CardHeader style={{ background: "#0d2c38", color: "#ffffff" }}>Why You Should Hire Me</CardHeader>
                                 <CardBody>
                                     <h2 id="sitterSummary">
                                         "{this.props.location.state.summary}"
@@ -97,16 +110,28 @@ class SitterProfile extends Component {
                                 </CardBody>
                             </Card>
                             <Card className="z-depth-5" id="favoriteCard">
-                                <CardHeader style={{ background: "#0d2c38", color: "#ffffff"}}>More Information</CardHeader>
+                                <CardHeader style={{ background: "#0d2c38", color: "#ffffff" }}>More Information</CardHeader>
                                 <CardBody>
-                                    <h2 className="profileSubtitle">City: {this.props.location.state.city}</h2> 
-                                    <h2 className="profileSubtitle">Years of Experience: {this.props.location.state.yearsOfExperience} years</h2> 
-                                    <h2 className="profileSubtitle">Types of Animals: {this.props.location.state.typesOfAnimals}</h2> 
-                                    <h2 className="profileSubtitle">Availability: {this.props.location.state.availability}</h2> 
+                                    <h2 className="profileSubtitle">City: {this.props.location.state.city}</h2>
+                                    <h2 className="profileSubtitle">Years of Experience: {this.props.location.state.yearsOfExperience} years</h2>
+                                    <h2 className="profileSubtitle">Types of Animals: {this.props.location.state.typesOfAnimals}</h2>
+                                    <h2 className="profileSubtitle">Availability: {this.props.location.state.availability}</h2>
                                 </CardBody>
                             </Card>
                         </Col>
                     </Row>
+                    <Modal isOpen={this.state.modal} toggle={this.toggle} className="text-white">
+                        <ModalHeader toggle={this.toggle} id="modalheaderProfile">Contact Information for {this.props.location.state.name}</ModalHeader>
+                        <ModalBody>
+                            <h1 id="contactTitles"><Fa size="1x" icon="envelope" className="ml-1"/> Email: </h1>
+                            <h1><Badge color="warning" pill>{this.props.location.state.email}</Badge></h1>
+                            <h1 id="contactTitles"><Fa size="1x" icon="mobile-phone" className="ml-1"/> Phone Number: </h1>
+                            <h1><Badge color="warning" pill>{this.props.location.state.phone}</Badge></h1>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button id="modalButtonProfile" onClick={this.toggle}>Close</Button>{' '}
+                        </ModalFooter>
+                    </Modal>
                 </Grid>
             </div>
         )
