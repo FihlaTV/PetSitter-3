efac9356c77448d1715fd38759d1480da6c3c26e
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-10-23 23:25:39 -0400

    successfully added userName to Favorites title

--- client/src/pages/Favorites/Favorites.js
+++ client/src/pages/Favorites/Favorites.js
@@ -13,6 +13,7 @@ class Favorites extends Component {
 
     state = {
         userId: "5bcfa7d189d2b336c4cf03c4",
+        userName: "Test",
         modal: false,
         sitters: []
     };
@@ -29,9 +30,11 @@ class Favorites extends Component {
         var decoded = jwt_decode(base64Url)
         console.log(decoded);
         this.setState({
-            userId: decoded.id
+            userId: decoded.id,
+            userName: decoded.name
         }, () => {
             console.log(this.state);
+            console.log(this.state.userName);
             this.loadSitters();
         })
 
@@ -71,14 +74,14 @@ class Favorites extends Component {
                                             <p><Image src={sitter.profilePhoto} alt="profile pic" size="medium" className="img-fluid hoverable mx-auto d-block" circular /></p><br />
                                         </Col>
                                         <Col size="sm-12 md-3" style={{ textAlign: "center" }}>
-                                            <h2>{sitter.name}, {sitter.age}</h2>
+                                            <h2 id="info">{sitter.name}</h2>
                                             <Rating maxRating={5} defaultRating={sitter.rating} icon='star' size='massive' disabled />
                                         </Col>
                                         <Col size="sm-12 md-3">
-                                            <h2>$29/night</h2>
+                                            <h2 id="info">${sitter.price}/per night</h2>
                                         </Col>
                                         <Col size="sm-12 md-3">
-                                            <Button animated id="contactsitterButton" className="mx-auto d-block" style={{ marginBottom: "100px" }} onClick={() => this.loadSitterProfile(sitter)}>
+                                            <Button animated id="sitterButton" className="mx-auto d-block" style={{ marginBottom: "100px" }} onClick={() => this.loadSitterProfile(sitter)}>
                                                 <Button.Content visible>Go to {sitter.name}'s Profile</Button.Content>
                                                 <Button.Content hidden>
                                                     <Icon name='arrow alternate circle right outline' size="large"/>
@@ -151,7 +154,7 @@ class Favorites extends Component {
                     </Row>
                     <Row>
                         <Col size="sm-10 md-12">
-                            <h1 id="favoritestitle">Favorites</h1>
+                            <h1 id="favoritestitle">{this.state.userName}'s Favorites</h1>
                         </Col>
                     </Row>
                     {this.renderSitters()}
